1a93af50c06810c3321e712850a2c533b923a2f2
Matthew Phillips <matthew@matthewphillips.info>
2023-02-17 17:51:26 -0500

    Use base config

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  base: '/comic-reader/',
+  site: 'https://pkg.spooky.click/comic-reader/',
+  trailingSlash: 'always',
   markdown: {
     syntaxHighlight: 'prism'
   }
